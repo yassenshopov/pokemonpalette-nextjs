0069e527aab7f4cc45f7f88be1522d0f3de5917a
Yassen Shopov <yassen.shopov@nuvei.com>
2024-12-13 12:33:26 +0200

    Update ESLint configuration: Disable specific rules for TypeScript and React to streamline development.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,13 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    rules: {
+      '@typescript-eslint/no-unused-vars': 'off',
+      'react/no-unescaped-entities': 'off',
+      'react-hooks/exhaustive-deps': 'off',
+    },
+  },
 ];
 
 export default eslintConfig;
